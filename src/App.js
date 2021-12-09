519551efd212d3f43c9453fb552960a8085a2147
reyhanehgz <buttercup_4442@yahoo.com>
2021-12-10 01:38:40 +0330

    add files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <footer>
+      This project was coded by {" "}
+          <a
+            href="https://"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Reyhaneh Gorjizadeh
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://s"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+      </footer>
+    </div>
+  );
+}
+
+
