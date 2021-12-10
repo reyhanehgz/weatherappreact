1e675cf53779595250c41b51210e22af2d470bef
reyhanehgz <buttercup_4442@yahoo.com>
2021-12-10 03:55:14 +0330

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,34 +3,35 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <footer>
-      This project was coded by {" "}
+      <div>
+        <header className="App-header">
+        </header>
+        <footer> This project was coded by {" "}
           <a
-            href="https://"
-            target="_blank"
-            rel="noopener noreferrer"
+              href="https://"
+              target="_blank"
+              rel="noopener noreferrer"
           >
-            Reyhaneh Gorjizadeh
+              Reyhaneh Gorjizadeh
           </a>{" "}
-          and is{" "}
+            and is{" "}
           <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noopener noreferrer"
+              href="https://github.com/"
+              target="_blank"
+              rel="noopener noreferrer"
           >
-            open-sourced on GitHub
+              open-sourced on GitHub
           </a>{" "}
-          and{" "}
+            and{" "}
           <a
-            href="https://s"
-            target="_blank"
-            rel="noopener noreferrer"
+              href="https://s"
+              target="_blank"
+              rel="noopener noreferrer"
           >
-            hosted on Netlify
+              hosted on Netlify
           </a>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
